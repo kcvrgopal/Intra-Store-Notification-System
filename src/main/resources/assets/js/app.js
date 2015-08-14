53f3e0b6a7c35e5dfef05abd20691c3da13ac0c1
RAJAGOPALAN VENKATARAMANAN <mailmerajagopalan@gmail.com>
2015-08-14 10:16:26 -0700

    Adding Version 1

---
+++ src/main/resources/assets/js/app.js
@@ -0,0 +1,120 @@
+
+$("#btn_Submit").click(function() {
+    //var isbn = this.id;
+	var user= $("#username").val();
+	var pass=$("#password").val();
+    $.ajax({
+    url: "/v1/users/login",
+    type: 'POST',
+    async: 'false',
+    data:'username='+user+'&password='+pass,
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(response) {
+    	//alert(response.username);
+    	$('#usrnm').text(response.username);
+    	$('#deptnm').text(response.department);
+    	//alert("hell"+$('#usrnm').text()+"HELL"+$('#deptnm').text());
+    	
+
+    $("#Admin").show();
+	 $("#login").hide();
+	 if(location.port==9001)
+
+	 {
+
+	 $("#delete").hide();
+
+	 }
+	 
+	fireWater();
+	 fireWater2();
+	 //storeListener();
+	 
+    },
+    error: function(data,status,er){
+    	    	
+    	alert("You are Invalid User!!!Please enter Correct Credentials");
+    	console.log("error ", data.error+""+status)}
+    	
+    });
+});
+
+$("#btn_Send").click(function() {
+    $.ajax({
+    url: "/v1/users/storeEmail",
+    type: 'GET',
+    async: 'false',
+    contentType: 'application/json',
+    success: function(response) {
+    	 
+    },
+    error: function(data,status,er){
+    }
+    });
+});
+$("#deptbtn_Send").click(function() {
+    var department= $("#txt_dept").val();
+	$.ajax({
+    url: "/v1/users/deptEmail",
+    type: 'POST',
+    async: 'false',
+    data:'department='+department,
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(response) {
+    	 
+    },
+    error: function(data,status,er){
+    }
+    });
+});
+$("#personbtn_Send").click(function() {
+    var username= $("#txt_person").val();
+	$.ajax({
+    url: "/v1/users/personEmail",
+    type: 'POST',
+    async: 'false',
+    data:'username='+username,
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(response) {
+    	 
+    },
+    error: function(data,status,er){
+    }
+    });
+});
+
+
+$("#btn_Reg").click(function() {
+	
+	$("#login_form").attr("action","/register");
+	
+});
+
+
+$("#reg_Submit").click(function() {
+    //var isbn = this.id;
+	var user= $("#username").val();
+	var pass=$("#password").val();
+	var fname=$("#firstname").val();
+	var lname=$("#lastname").val();
+	var email=$("#email").val();
+	var phno=$("#phno").val();
+	var dept=$("#dept").val();	
+   
+    $.ajax({
+    url: "v1/users",
+    type: 'POST',
+    async: 'false',
+    data:'username='+user+'&password='+pass+'&firstname='+fname+'&lastname='+lname+'&email='+email+'&phno='+phno+'&dept='+dept,
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(response) {
+   
+   
+    },
+    error: function(data,status,er){
+    	
+    	 alert("You are Successfully  Registered!");
+    }
+    	
+    });
+});
---
+++ src/main/resources/assets/js/controller.js
@@ -0,0 +1,85 @@
+function deptClicked(){
+$("#deptWide").show();
+$("#streWide").hide();
+$("#person").hide();
+}
+function personClicked(){
+	$("#deptWide").hide();
+	$("#streWide").hide();
+	$("#person").show();
+	
+	
+	}
+function storeClicked(){
+	$("#deptWide").hide();
+	$("#streWide").show();
+	$("#person").hide();
+	
+	
+	}
+	
+function deleteUser(){
+	var lname = document.getElementById("usernameDelete").value;
+	alert(lname + " is about to be deleted");
+	var URL = "/v1/users/all/users/v1/delete/"+lname;
+	 $.ajax({
+	 url: URL,
+	 type: 'DELETE',
+	 async: 'false',
+	 contentType: 'application/x-www-form-urlencoded',
+	 success: function(response) {
+	 alert("You have Successfully  Deleted!");
+		window.location.reload();
+	 },
+	 error: function(data,status,er){
+	 	
+	 	
+	 	alert("Unable to Delete at this moment!!!");
+	 	}
+
+	});
+}	
+	
+function searchUserByDept(){
+
+	var dname = $('#deptnm').text();
+
+	var URL = "/v1/users/display/"+dname;
+
+	$.ajax({
+
+	url: URL,
+
+	type: 'GET',
+
+	async: 'false',
+
+	contentType: 'application/x-www-form-urlencoded',
+
+	success: function(response) {
+
+	var names = new Array();
+
+	for(var i=0; i<response.username.length;i++)
+
+	{
+
+	names[i]=response.username[i];
+
+	}
+
+	alert("The Users in the "+dname+" Department are : "+names);
+
+	},
+
+	error: function(data,status,er){
+
+	alert("Unable to Delete at this moment!!!");
+
+	}
+
+	 
+
+	});
+
+	}
